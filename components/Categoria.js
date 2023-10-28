5b0cd6cbeb8237104b4ea5e3e3d97f24a299dd67
Fede <fede.alejandro87@gmail.com>
2023-10-28 12:48:35 +0200

    agregado botón en categorías

--- components/Categoria.js
+++ components/Categoria.js
@@ -7,18 +7,18 @@ const Categoria = ({ categoria }) => {
     const { nombre, icono, id } = categoria;
 
     return (
-        <div
-            className={`${categoriaActual?.id === id ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border p-5 hover:bg-amber-400`}>
-            <Image
-                width={70}
-                height={70}
-                src={`/assets/img/icono_${icono}.svg`}
-                alt='Imagen icono'
-            />
+        <div>
             <button
                 type='button'
-                className='text-2xl font-bold hover:cursor-pointer'
-                onClick={() => handleClickCategoria(id)}>
+                onClick={() => handleClickCategoria(id)}
+                className={`${categoriaActual?.id === id ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border p-5 hover:bg-amber-400 text-2xl font-bold hover:cursor-pointer`}>
+                <Image
+                    width={70}
+                    height={70}
+                    src={`/assets/img/icono_${icono}.svg`}
+                    alt='Imagen icono'
+                />
+
                 {nombre}
             </button>
         </div>
